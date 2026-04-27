4cc262dcdabbdd7c99dd7ed9f5c17814fc770b09
janlucaaa <jan.telesklav@itlabs.at>
2026-04-27 13:48:37 +0200

    Removed wwww from vel.y

--- Assets/Scripts/character.gd
+++ Assets/Scripts/character.gd
@@ -21,7 +21,7 @@ func _physics_process(delta: float) -> void:
 		vel.y = jump_velocity
 
 	if Input.is_action_just_released("ui_up") and vel.y < 0.0:
-		vel.y *= 0.5wwww
+		vel.y *= 0.5
 
 	var direction: float = Input.get_axis("ui_left", "ui_right")
 
