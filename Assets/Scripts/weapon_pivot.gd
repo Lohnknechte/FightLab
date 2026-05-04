a0a7841ae101f7de878dad7b5aa94baa3bf5a8ed
Halloplayer <halloplayer7@gmail.com>
2026-05-04 14:17:06 +0200

    Weapon control system finished

---
+++ Assets/Scripts/weapon_pivot.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var _sprite: Sprite2D
+var _character: CharacterBody2D
+
+var originalPosX : float
+var originalPosY : float
+
+func _ready() -> void:
+	_sprite = $Sprite
+	_character = get_parent()
+	
+	originalPosX = self.position.x
+	originalPosY = self.position.y
+
+func _process(delta: float) -> void:
+	var mouse_pos = get_global_mouse_position()
+	var to_mouse = mouse_pos - global_position
+	
+	$Sprite.flip_v = to_mouse.x < 0
+	self.position.x = -originalPosX if _character.facing_left else originalPosX
+	
+	look_at(mouse_pos)
